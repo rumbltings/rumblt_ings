239acd8586b45dee97d55ee1149ae154158cb915
robyn <robyn@s56.net>
2018-06-25 14:45:17 -0600

    boop

---
+++ rumblt/src/robyn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function boop () {
+    return (
+        <div>
+            We should probably delete this once we are done messing
+        </div>
+    )
+}
