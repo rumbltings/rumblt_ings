14023b6cf591698fe95beea65fbf954d06045585
Tarin Strosser <strawsir@gmail.com>
2018-06-25 14:36:14 -0600

    Tarin's second commit

--- rumblt/src/App.js
+++ rumblt/src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           
           <h1 className="App-title">Hello! Tarin Added This!</h1>
+          <h2>Tarin ALSO added this!!!</h2>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
